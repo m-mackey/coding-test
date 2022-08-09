322d24790eefb5bbc1cbc9b85b45bdcf00343d4a
Megan Mackey <megan.mackey24@gmail.com>
2022-08-09 01:28:18 -0500

    Basic setting up of html sections

--- scripts.js
+++ scripts.js
@@ -12,31 +12,6 @@ goalInput.addEventListener('submit', (e) => {
   goal.textContent = display;
 });
 
-// function addToTotalSaved(newDeposit) {
-//   //   const totalSaved = Number(document.getElementById('total-saved').textContent);
-//   //   console.log(totalSaved);
-
-//   //   const newTotal = Number(totalSaved) + newDeposit;
-//   //   console.log(newTotal);
-//   //   totalSaved.textContent = newTotal;
-//   const newTotal = Number(totalSaved) + newDeposit;
-//   console.log(newTotal);
-//   //   console.log(`total saved is${totalSaved}`);
-//   const showNewTotal = `${newTotal}`;
-//   console.log(`this is${showNewTotal}`);
-//   totalSaved.textContent = showNewTotal;
-//   console.log(totalSaved);
-// }
-
-depositInput.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const depositAmount = Number(this.depositAmount.value);
-  const newTotal = Number(totalSaved.textContent) + depositAmount;
-  const showNewTotal = `${newTotal}`;
-  console.log(showNewTotal);
-  totalSaved.textContent = showNewTotal;
-});
-
 function checkTotals() {
   const currentGoal = Number(goal.textContent);
   const currentSaved = Number(totalSaved.textContent);
@@ -47,7 +22,14 @@ function checkTotals() {
   }
 }
 
-checkTotals();
+depositInput.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const depositAmount = Number(this.depositAmount.value);
+  const newTotal = Number(totalSaved.textContent) + depositAmount;
+  const showNewTotal = `${newTotal}`;
+  console.log(showNewTotal);
+  totalSaved.textContent = showNewTotal;
+  checkTotals();
+});
 
 // maybe put amount to save in localstorage so it doesnt reset on load when u update
- 
